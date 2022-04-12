f0aef06ce450def960c4ce602c69f5ddde3dc295
yunjae <juj7347@naver.com>
2022-04-12 16:25:55 +0900

    aop time code

--- src/main/java/hello/hellospring/aop/TimeTraceApp.java
+++ src/main/java/hello/hellospring/aop/TimeTraceApp.java
@@ -3,11 +3,13 @@ package hello.hellospring.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class TimeTraceApp {
 
-    @Around()
+    @Around("execution(* hello.hellospring..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
         System.out.println("START: " + joinPoint.toString());
