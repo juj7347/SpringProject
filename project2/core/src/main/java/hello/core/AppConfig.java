8e84ac68eaf712d0906db34fcdd8c45714516d00
yunjae <juj7347@naver.com>
2022-04-14 00:15:20 +0900

    refactored to use spring

--- project2/core/src/main/java/hello/core/AppConfig.java
+++ project2/core/src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,38 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
-    public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    @Bean
+    public MemberService memberService() { //MemberService 역할
+        return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    @Bean
+    public MemberRepository memberRepository() { //MemberRepository 역할
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() { //OrderService 역할
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //return new RateDiscountPolicy();
+        return new FixDiscountPolicy();
     }
 }
